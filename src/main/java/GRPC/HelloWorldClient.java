30963973dd2e9bda006d156387269f38bb8f4623
AlenGaz <alengazibegovic@gmail.com>
2021-02-20 18:48:14 +0100

    test

---
+++ src/main/java/GRPC/HelloWorldClient.java
@@ -0,0 +1,104 @@
+package GRPC;
+
+import aima.core.util.datastructure.Pair;
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.hellos.Hello;
+import io.grpc.hellos.HelloWorldServiceGrpc;
+
+
+public class HelloWorldClient {
+
+    private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+    private final HelloWorldServiceGrpc.HelloWorldServiceBlockingStub blockingStub;
+
+  
+
+    // object using in cord
+    public GRPC.HelloWorldClient HelloWorldClient;
+
+    public HelloWorldClient(Channel channel){
+        //blockingStub = HelloWorldServiceGrpc.newBlockingStub(channel);
+        blockingStub = HelloWorldServiceGrpc.newBlockingStub(channel);
+    }
+
+    public void makeGreeting(String name){
+
+
+        Hello.HelloRequest request = Hello.HelloRequest.newBuilder().setName(name).build();
+        Hello.HelloResponse response;
+
+        try
+        {
+            System.out.println("making greeting with : " + name);
+            response = blockingStub.hello(request);
+        }
+        catch(StatusRuntimeException e){
+            logger.log(Level.WARNING, "Rpc Failed: {0}", e.getStatus());
+            return;
+        }
+        System.out.println("Logging the response of the server ...");
+        System.out.println(" Response is: "+ response.getName());
+    }
+
+
+
+    public void makeGreeting2(Pair<String,Integer> pair){
+
+
+
+        Hello.getRobotID getrobotid = Hello.getRobotID.newBuilder().setKey(pair.getFirst()).setValue(pair.getSecond()).build();
+        Hello.robotID robotid;
+
+        try
+        {
+            System.out.println("making greeting with: " + pair + pair.getFirst() );
+            robotid = blockingStub.grobotID(getrobotid);        ////////////////////////////////////////////////////////
+
+        }
+        catch(StatusRuntimeException e){
+            logger.log(Level.WARNING, "Rpc Failed: {0}", e.getStatus());
+            return;
+        }
+
+        System.out.println("Logging the response of the server ...");
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String messageToSend;
+        String target = "localhost:50051";
+
+
+        Random rd = new Random();
+
+        if(rd.nextFloat()> 0.5){
+            messageToSend = "Boss";
+        }
+        else{
+            messageToSend= "Hello";
+        }
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+
+        try{
+            HelloWorldClient client = new HelloWorldClient(channel);
+            client.makeGreeting((messageToSend));
+
+        }
+        finally{
+
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+
+    }
+
+}
---
+++ src/main/java/GRPC/HelloWorldServiceImpl.java
@@ -0,0 +1,85 @@
+package GRPC;
+
+import io.grpc.hellos.Hello;
+import io.grpc.hellos.HelloWorldServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class HelloWorldServiceImpl
+
+
+        extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
+
+
+    public int it = 0;
+    String text;
+    public List<Integer> ID_list = new ArrayList();
+    public int x;
+
+    @Override
+    public void hello(
+            Hello.HelloRequest request,
+            StreamObserver<Hello.HelloResponse> responseObserver) {
+        System.out.println("handling [" + request.getName() + "]"+ " endpoint: [" + request.toString() + "]");
+
+
+        if(request.getName().toString().equals("Hello")){
+
+            text = request.getName() + " World";
+            respondWithSendingMsg(responseObserver);
+        }
+
+        if(request.getName().toString().equals("Boss")) {
+            text = "Hello big" + request.getName();
+
+            respondWithSendingMsg(responseObserver);
+        }
+
+    }
+
+    @Override
+    public void grobotID(
+            Hello.getRobotID request,
+            StreamObserver<Hello.robotID> responseObserver) {
+        System.out.println("handling [" + request.getKey() + "]"+ " endpoint: [" + request.toString() + "]");
+
+        if(request.getKey().toString().equals("My ID")) {
+
+            text = "Hello robot:" + request.getValue();
+            System.out.println("got Robot id; " + request.getValue());
+            ID_list.add(request.getValue()); ////// if id not in list, append
+
+            respondWithSendingIDreceived(responseObserver);
+        }
+
+    }
+
+    private void respondWithSendingIDreceived(StreamObserver<Hello.robotID> responseObserver) {
+        Hello.robotID response =
+                Hello.robotID.newBuilder()
+                        .setName(text + "&id received" ).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+
+
+
+
+    private void respondWithSendingMsg(StreamObserver<Hello.HelloResponse> responseObserver) {
+        Hello.HelloResponse response =
+                Hello.HelloResponse.newBuilder()
+                        .setName(text + " " + it).build();
+        System.out.println("Responding with -> " + response.getName() + " !The # of clients is: " + ID_list.size());
+        //System.out.println("all current clients (robot ID:s) ; " +Arrays.toString(ID_list.toArray()));
+
+
+        it++;
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
